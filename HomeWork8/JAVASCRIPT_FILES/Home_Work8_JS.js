ccb3999a8516a96fe555f4cbe856a3cf32b6d0b0
Vytautas <vytautas.bartkus91@gmail.com>
2019-05-18 22:27:56 +0300

    Unfinished homework 8

---
+++ HomeWork8/JAVASCRIPT_FILES/Home_Work8_JS.js
@@ -0,0 +1,41 @@
+function myFunction1() {
+    document.getElementById("section-2-right-side").style.backgroundImage = "url(https://img.freepik.com/free-photo/shiny-yellow-leaf-gold-foil-texture_38679-522.jpg?size=626&ext=jpg)"
+    document.getElementById("button-1").style.backgroundColor = "transparent";
+    document.getElementById("button-2").style.backgroundColor = "black";
+    document.getElementById("button-3").style.backgroundColor = "black";
+    document.getElementById("button-4").style.backgroundColor = "black";
+    document.getElementById("button-5").style.backgroundColor = "black";
+
+}
+function myFunction2() {
+    document.getElementById("section-2-right-side").style.backgroundImage = "url(https://img.freepik.com/free-vector/blue-3d-modern-background-design_53876-88247.jpg?size=626&ext=jpg)"
+    document.getElementById("button-1").style.backgroundColor = "black";
+    document.getElementById("button-2").style.backgroundColor = "transparent";
+    document.getElementById("button-3").style.backgroundColor = "black";
+    document.getElementById("button-4").style.backgroundColor = "black";
+    document.getElementById("button-5").style.backgroundColor = "black";
+}
+function myFunction3() {
+    document.getElementById("section-2-right-side").style.backgroundImage = "url(https://img.freepik.com/free-photo/green-shade-abstract-watercolor-art-hand-paint-background_23-2148055449.jpg?size=626&ext=jpg)"
+    document.getElementById("button-1").style.backgroundColor = "black";
+    document.getElementById("button-2").style.backgroundColor = "black";
+    document.getElementById("button-3").style.backgroundColor = "transparent";
+    document.getElementById("button-4").style.backgroundColor = "black";
+    document.getElementById("button-5").style.backgroundColor = "black";
+}
+function myFunction4() {
+    document.getElementById("section-2-right-side").style.backgroundImage = "url(https://img.freepik.com/free-photo/soft-pink-wooden-background_24837-20.jpg?size=626&ext=jpg)"
+    document.getElementById("button-1").style.backgroundColor = "black";
+    document.getElementById("button-2").style.backgroundColor = "black";
+    document.getElementById("button-3").style.backgroundColor = "black";
+    document.getElementById("button-4").style.backgroundColor = "transparent";
+    document.getElementById("button-5").style.backgroundColor = "black";
+}
+function myFunction5() {
+    document.getElementById("section-2-right-side").style.backgroundImage = "url(https://gamblincolors.com/wp-content/uploads/2016/04/Cadmium-Orange-Deep-1.jpg)"
+    document.getElementById("button-1").style.backgroundColor = "black";
+    document.getElementById("button-2").style.backgroundColor = "black";
+    document.getElementById("button-3").style.backgroundColor = "black";
+    document.getElementById("button-4").style.backgroundColor = "black";
+    document.getElementById("button-5").style.backgroundColor = "transparent";
+}
