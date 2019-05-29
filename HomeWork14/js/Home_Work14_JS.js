fdf6ffde7138f4faf532e4a9d5052b76a40e56a6
Vytautas <vytautas.bartkus91@gmail.com>
2019-05-29 08:09:52 +0300

    Finished homework 14

--- HomeWork14/js/Home_Work14_JS.js
+++ HomeWork14/js/Home_Work14_JS.js
@@ -1,4 +1,6 @@
-var triangleTypeString = "";
+
+
+// Triangle calculation
 
 function isTriangle(a, b, c) {
     if((a + b > c) && (a + c > b) && (c + b > a)){
@@ -10,29 +12,26 @@ function isTriangle(a, b, c) {
 
 function triangleType (a, b, c) {
         if(isTriangle(a, b, c)){
-            if((a===b) && (a===c) && (b===c)){
-                triangleTypeString = "Equilateral triangle"
+            if(a===b && a===c && b===c){
+               return "Equilateral triangle";
             }
-            if(((a===b) || (a===c) || (b===c))){
-                triangleTypeString = "Isosceles triangle";
+            if((a===b) || (a===c) || (b===c)){
+                return "Isosceles triangle";
             }
             if((a*a + b*b === c*c) || (a*a + c*c === b*b) || (b*b + c*c === a*a)){
-                triangleTypeString = "Right triangle"
+                return "Right triangle";
             }
             else{
-                triangleTypeString = "Scalene triangle"
+                return  "Scalene triangle";
             }
         }
-        else{
-            triangleTypeString = "Not a triangle"
-        }
-        return triangleTypeString;
-    }
+        return  "Not a triangle";
+}
 
     function triangleArea(a,b,c) {
         var area = Math.sqrt(((a + b + c)/2) * ((a + b + c)/2 - a) * ((a + b + c)/2 - b) * ((a + b + c)/2 - c));
         if(!isTriangle(a,b,c)){
-            area = 0;
+            area = -1;
         }
         return area;
     }
@@ -48,3 +47,20 @@ function triangleType (a, b, c) {
 }
 
 
+// Speed calculation
+
+var car = ["2017-07-06 19:59:45", "LRS123", 5000, 118];
+var speed = 0.0;
+
+function carSpeed() {
+    speed = (car[2]/1000)/(car[3]/60/60);
+    return Math.round(speed);
+}
+
+function displayCarAttributes() {
+    document.getElementById("date").innerHTML = car[0];
+    document.getElementById("pl-number").innerHTML = car[1];
+    document.getElementById("distance").innerHTML = car[2];
+    document.getElementById("time").innerHTML = car[3];
+    document.getElementById("speed").innerHTML = carSpeed();
+}
