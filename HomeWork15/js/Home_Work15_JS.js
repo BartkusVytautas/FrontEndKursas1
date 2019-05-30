ca7ca1ad238ab9de9f311a33ea461459540246ab
Vytautas <vytautas.bartkus91@gmail.com>
2019-05-30 23:41:29 +0300

    Unfinished homework 15

--- HomeWork15/js/Home_Work15_JS.js
+++ HomeWork15/js/Home_Work15_JS.js
@@ -1,15 +1,30 @@
-function drop_handler(ev) {
+var drop = function drop_handler(ev) {
     ev.preventDefault();
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
     ev.dataTransfer.clearData();
 }
 
-function dragover_handler(ev) {
+var dragover = function dragover_handler(ev) {
     ev.preventDefault();
 }
 
-function dragstart_handler(ev) {
+var dragstart = function dragstart_handler(ev) {
     ev.dataTransfer.setData("text/plain", ev.target.id);
 }
+
+function set_global_attributes_onLoad (){
+    var iconArray = document.getElementsByTagName('i');
+    var squareArray = document.getElementsByTagName('td');
+    for (var i =0; i<iconArray.length; i++){
+        iconArray[i].draggable = true;
+        iconArray[i].ondragstart = dragstart;
+    }
+    for(var i = 0; i < squareArray.length; i++){
+        squareArray[i].ondrop = drop;
+        squareArray[i].ondragover = dragover;
+    }
+}
+
+
 
