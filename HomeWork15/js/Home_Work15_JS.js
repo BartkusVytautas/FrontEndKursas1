da041596b99df1cb59928fabecab6db19f113f93
Vytautas <vytautas.bartkus91@gmail.com>
2019-05-30 14:04:33 +0300

    Unfinished homework 15

--- HomeWork15/js/Home_Work15_JS.js
+++ HomeWork15/js/Home_Work15_JS.js
@@ -1,13 +1,15 @@
-function allowDrop(ev) {
+function drop_handler(ev) {
     ev.preventDefault();
-}
-
-function drag(ev) {
-    ev.dataTransfer.setData("Text", ev.target.id);
-}
-
-function drop(ev) {
-    var data = ev.dataTransfer.getData("Text");
+    var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
+    ev.dataTransfer.clearData();
+}
+
+function dragover_handler(ev) {
     ev.preventDefault();
 }
+
+function dragstart_handler(ev) {
+    ev.dataTransfer.setData("text/plain", ev.target.id);
+}
+
