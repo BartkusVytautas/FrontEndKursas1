f534bcc8efcbfcec4e5295fffad37124f53b662d
Vytautas <vytautas.bartkus91@gmail.com>
2019-05-31 08:53:58 +0300

    Unfinished homework 15

--- HomeWork15/js/Home_Work15_JS.js
+++ HomeWork15/js/Home_Work15_JS.js
@@ -1,8 +1,13 @@
+var squareArray = [];
+
+
 var drop = function drop_handler(ev) {
     ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-    ev.dataTransfer.clearData();
+
+            var data = ev.dataTransfer.getData("text");
+            ev.target.appendChild(document.getElementById(data));
+            ev.dataTransfer.clearData();
+
 }
 
 var dragover = function dragover_handler(ev) {
@@ -10,20 +15,26 @@ var dragover = function dragover_handler(ev) {
 }
 
 var dragstart = function dragstart_handler(ev) {
+
     ev.dataTransfer.setData("text/plain", ev.target.id);
 }
 
-function set_global_attributes_onLoad (){
+function set_global_attributes_onLoad () {
     var iconArray = document.getElementsByTagName('i');
-    var squareArray = document.getElementsByTagName('td');
-    for (var i =0; i<iconArray.length; i++){
-        iconArray[i].draggable = true;
-        iconArray[i].ondragstart = dragstart;
-    }
-    for(var i = 0; i < squareArray.length; i++){
-        squareArray[i].ondrop = drop;
-        squareArray[i].ondragover = dragover;
-    }
+    var tdArray = document.getElementsByTagName('td');
+
+    for (var j = 0; j < tdArray.length; j++) {
+        if (!((j === 0) || (j % 9 === 0) || (j > 72))) {
+            tdArray[j].ondrop = drop;
+            tdArray[j].ondragover = dragover;
+            squareArray.push(tdArray[j]);
+                for (var i = 0; i < iconArray.length; i++) {
+                    iconArray[i].draggable = true;
+                    iconArray[i].ondragstart = dragstart;
+                    }
+            }
+        }
+
 }
 
 
